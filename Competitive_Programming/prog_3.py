c0a18d4a9104f472a446835bc109cdcb8804c5b8
vineetkr7 <shubham2vineet@gmail.com>
2021-01-09 06:02:22 +0530

    Updated example input/output
    Need to work on input 3

--- Competitive_Programming/prog_3.py
+++ Competitive_Programming/prog_3.py
@@ -8,21 +8,29 @@
 # • X is the prefix array of array A and Y is the suffix array of array A.
 # Note: Array X or Y can be empty.
 
-
 # Problem Constraints
 
 #     1 <= len(A) <= 10^5
 #     1 <= A[i] <= 10^9
 
-
 # Input Format
 
 # First and only argument is an integer array Array.
 
-
 # Output Format
 
 # Return an integer array denoting lexicographically largest array S.
+
+# Example Input                                                                       Example Output
+
+# Input 1:                                                                            Output 1:
+#   A = [4, 1, 3, 2]                                                                    [4, 2, 3, 1]
+
+# Input 2:                                                                            Output 2:
+#   A = [ 10, 20, 30, 40 ]                                                              [40, 30, 20, 10]
+
+# Input 3:                                                                            Output 3:
+#   A = [ 20, 69, 4, 83, 23, 100, 63, 50, 85, 53, 40, 44, 1 ]                           [20, 69, 4, 83, 23, 100, 63, 50, 85, 53, 40, 44, 1]
 
 
 import ast
